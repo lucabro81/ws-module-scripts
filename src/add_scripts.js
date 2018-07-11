66eebbefd5cbe30033a0b5843029a06816884063
Luca <lucabro_2000@yahoo.it>
2018-07-11 22:27:30 +0200

    minor mod

--- src/add_scripts.js
+++ src/add_scripts.js
@@ -6,9 +6,9 @@ let cp = require('child_process');
 let current_path_arr = process.cwd().split("/");
 let local_path = [];
 
-for (let i = 0; i < path_arr.length; i++) {
-    if (path_arr[i] === "node_modules") {
-        local_path.push(path_arr[i]);
+for (let i = 0; i < current_path_arr.length; i++) {
+    if (current_path_arr[i] === "node_modules") {
+        local_path.push(current_path_arr[i]);
     }
     else {
         local_path.push("package.json");
