f56b155968a339306c3ac7ecdee30ba5bb423084
Luca <lucabro_2000@yahoo.it>
2018-07-12 00:30:29 +0200

    minor mod

--- src/add_scripts.js
+++ src/add_scripts.js
@@ -1,13 +1,10 @@
 #!/usr/bin/env node
 
 let fs = require('fs');
-let cp = require('child_process');
 
 let current_path_arr = process.cwd().split("/");
 let local_path_arr = [];
 let local_path = "";
-
-console.log("current_path_arr: ", current_path_arr);
 
 for (let i = 0; i < current_path_arr.length; i++) {
     if (current_path_arr[i] !== "node_modules") {
@@ -20,14 +17,27 @@ for (let i = 0; i < current_path_arr.length; i++) {
     }
 }
 
-console.log("local_path: ", local_path);
+let package_json = fs.readFileSync(local_path, 'utf8');
+let package_json_arr = package_json.split('\n');
+let package_json_mod = "";
 
-let package_json = require(local_path);
+let scripts_found = false;
 
-console.log("package_json: ", package_json);
+for (let i = 0; i < package_json_arr.length; i++) {
+    if (package_json_arr[i].includes("scripts")) {
+        scripts_found = true;
+    }
+    else if (scripts_found && package_json_arr[i].includes("}")) {
+        package_json_mod +=
+            "\t\t\"add-web-service\": \"add_web_service\"\n" +
+            "\t\t\"mod-web-service\": \"mod_web_service\"\n" +
+            "\t}\n";
+    }
+    else {
+        package_json_mod += package_json_arr[i];
+    }
+}
 
-package_json["scripts"]["scriptaggiunto"] = "assd";
-
-fs.writeFileSync(process.cwd() + "/" + "package.json", JSON.stringify(package_json), 'utf8');
+fs.writeFileSync(local_path, package_json_mod, 'utf8');
 
 process.exit();
