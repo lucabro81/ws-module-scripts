11a1008ac322aa00092ee9c60d7213698f66efd9
Luca <lucabro_2000@yahoo.it>
2018-07-11 22:08:45 +0200

    minor mod

--- src/add_scripts.js
+++ src/add_scripts.js
@@ -5,8 +5,10 @@ let cp = require('child_process');
 
 let package_json = require(process.cwd() + "/" + "package.json");
 
+console.log(process.cwd() + "/" + "package.json", $INIT_CWD);
+
 package_json["scripts"]["scriptaggiunto"] = "assd";
 
-fs.writeFileSync(process.cwd() + "/" + "package.json", package_json, 'utf8');
+// fs.writeFileSync(process.cwd() + "/" + "package.json", package_json, 'utf8');
 
 process.exit();
